5c6dde3860ff25353c253f799c7dc55620f5c058
TejeshwarReddy <tejeshwarreddy1996@gmail.com>
2017-03-13 21:01:14 +0530

    Adds OnBootUp RESUME RECORDING.
    Adds Shared preference for recording.

--- app/src/main/java/project/bits/com/recandup/VideoPrefs.java
+++ app/src/main/java/project/bits/com/recandup/VideoPrefs.java
@@ -15,6 +15,7 @@ public class VideoPrefs {
 
     private static final String PACKAGE_NAME = "VIDEO_PREFS";
     private static final String quality = "quality";
+    private static final String RECORDING_STATUS = "status";
 
     private static SharedPreferences getPrefs(Context context) {
         return context.getSharedPreferences(PACKAGE_NAME,Context.MODE_PRIVATE);
@@ -27,4 +28,13 @@ public class VideoPrefs {
     public static void setQuality(Context context,int quality) {
         getPrefs(context).edit().putInt(VideoPrefs.quality,quality).apply();
     }
+
+    public static void setVideoRecordingStatus(Context context,int status){
+        getPrefs(context).edit().putInt(VideoPrefs.RECORDING_STATUS,status).apply();
+    }
+
+    public static int getVideoRecordingStatus(Context context){
+        return getPrefs(context).getInt(RECORDING_STATUS,0);
+    }
+
 }
